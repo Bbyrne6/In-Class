97c75ae2cc50f0651513300a5cc2c7397c541daf
Bbyrne6 <briannajanelleb@gmail.com>
2014-09-19 09:22:39 -0600

    Correct E Resturaunt File

--- EResturaunt/EResturaunt/eRestaurant.Requirments/UserStories/ManageWaitersStory.cs
+++ EResturaunt/EResturaunt/eRestaurant.Requirments/UserStories/ManageWaitersStory.cs
@@ -4,9 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace eRestaurant.Requirments
+namespace eRestaurant.Requirments.UserStories
 {
-    class ManageWaitersStory
+    [story(AsA="Resturaunt manager",
+Iwant="To manage my waiters",
+SoThat="I have enough staff to wait on tables",
+)]
+
+}
+public class ManageWairersStory
+{
+[fact]
+[AutoRollback]
+public void AddWaitersScenerio() 
+{
+	Waiter newGuy = new Waiter();
+int waiterId = -1;
+	this.Given(_=>GiveWaiterInformation(newGuy))
+.when(_=>WhenIAddTheWaiter(newGuy, out waitId))
+.then(_=>ThenTheWaiterExists(waiterId))
+.And(_=>TheWaiterDetailsMatch(waiterId, newGuy))
+.BDDfy
+
+private object TheWaiterDetailsMatch(int waiterId,Waiter newGuy)
+{
+ 	throw new NotImplementedException();
+}
+
+private object ThenTheWaiterExists(int waiterId)
+{
+ 	throw new NotImplementedException();
+}
+
+private object GiveWaiterInformation(Waiter newGuy)
+{
+ 	throw new NotImplementedException();
+} ManageWaitersStory
     {
     }
 }
